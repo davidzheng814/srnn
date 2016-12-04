699edd7d16bf8d194bf29c9bcad717101f0fa70f
David Zheng <dzd123@mit.edu>
2016-12-03 19:19:07 -0500

    Update model to be more modular.

--- model.py
+++ model.py
@@ -13,9 +13,10 @@ from collections import namedtuple
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-NUM_EPOCHS = 200
-BATCH_SIZE = 5
-MAX_TIME = 25
+NUM_EPOCHS = 100
+BATCH_SIZE = 10
+MAX_TIME = 150
+MAX_FILES = None
 
 LEARNING_RATE = .05
 BETA_1 = 0.9
@@ -27,14 +28,26 @@ LOGS_DIR = 'logs/'
 DATA_DIR = 'numpy_arrays/'
 TRAIN_RATIO = 0.75
 
-HT_INP_SIZE = 160
-OT_INP_SIZE = 40
-HO_INP_SIZE = 400
-OO_INP_SIZE = 400
-H_INP_SIZE = 630
-O_INP_SIZE = 180
-H_OUT_SIZE = 10
-O_OUT_SIZE = 12
+Node = namedtuple('Node', ['name', 'inp_size', 'time_size', 'out_size'])
+NODES = [
+    Node('s', 18, 36, 18),
+    Node('a', 24, 48, 24),
+    Node('l', 15, 30, 15),
+]
+
+Edge = namedtuple('Edge', ['node1', 'node2', 'size'])
+EDGES = [
+    [
+        Edge('s', 'a', 42),
+        Edge('s', 'l', 33),
+    ], [
+        Edge('a', 's', 42),
+        Edge('a', 'a', 48),
+    ], [
+        Edge('l', 's', 33),
+        Edge('l', 'l', 30),
+    ]
+]
 
 def pad(x):
     r = np.zeros([x.shape[0], MAX_TIME, x.shape[2]])
@@ -42,41 +55,58 @@ def pad(x):
 
     return r
 
+def get_name(edge, sort=True):
+    if edge.node1 > edge.node2 and sort:
+        edge.node2 + '_' + edge.node1
+    return edge.node1 + '_' + edge.node2
+
 class Loader(object):
     def __init__(self, file_list):
         self.file_list = file_list
         self.load_dataset()
 
     def load_dataset(self):
-        """
-        Sets ht_inputs, ot_inputs, ho_inputs, oh_inputs, oo_inputs,
-            h_inputs, o_inputs, h_outputs, o_outputs
-        """
-        self.ht_inputs = np.zeros([0, MAX_TIME, HT_INP_SIZE])
-        self.ot_inputs = np.zeros([0, MAX_TIME, OT_INP_SIZE])
-        self.ho_inputs = np.zeros([0, MAX_TIME, HO_INP_SIZE])
-        self.oh_inputs = np.zeros([0, MAX_TIME, HO_INP_SIZE])
-        self.oo_inputs = np.zeros([0, MAX_TIME, OO_INP_SIZE])
-        self.h_inputs = np.zeros([0, MAX_TIME, H_INP_SIZE])
-        self.o_inputs = np.zeros([0, MAX_TIME, O_INP_SIZE])
-        self.h_outputs = np.zeros([0, MAX_TIME, H_OUT_SIZE])
-        self.o_outputs = np.zeros([0, MAX_TIME, O_OUT_SIZE])
+        """"""
+        self.node_inputs = []
+        self.time_inputs = []
+        self.node_outputs = []
+        self.edge_inputs = []
+        for i, node in enumerate(NODES):
+            self.node_inputs.append(np.zeros([0, MAX_TIME, node.inp_size]))
+            self.time_inputs.append(np.zeros([0, MAX_TIME, node.time_size]))
+            self.node_outputs.append(np.zeros([0, MAX_TIME, node.out_size]))
+
+            self.edge_inputs.append([])
+            for edge in EDGES[i]:
+                self.edge_inputs[-1].append(np.zeros([0, MAX_TIME, edge.size]))
+
         for filename in self.file_list:
             data = np.load(filename)
-            self.ht_inputs = np.append(self.ht_inputs, pad(data['ht_inputs']), axis=0)
-            self.ot_inputs = np.append(self.ot_inputs, pad(data['ot_inputs']), axis=0)
-            self.ho_inputs = np.append(self.ho_inputs, pad(data['ho_inputs']), axis=0)
-            self.oh_inputs = np.append(self.oh_inputs, pad(data['oh_inputs']), axis=0)
-            self.oo_inputs = np.append(self.oo_inputs, pad(data['oo_inputs']), axis=0)
-            self.h_inputs = np.append(self.h_inputs, pad(data['h_inputs']), axis=0)
-            self.o_inputs = np.append(self.o_inputs, pad(data['o_inputs']), axis=0)
-            self.h_outputs = np.append(self.h_outputs, pad(data['h_outputs']), axis=0)
-            self.o_outputs = np.append(self.o_outputs, pad(data['o_outputs']), axis=0)
+
+            for i, node in enumerate(NODES):
+                self.node_inputs[i] = \
+                    np.append(self.node_inputs[i], pad(data[node.name + '_inp']), 
+                              axis=0)
+
+                self.time_inputs[i] = \
+                    np.append(self.time_inputs[i], pad(data[node.name + '_time']),
+                              axis=0)
+                    
+                self.node_outputs[i] = \
+                    np.append(self.node_outputs[i], pad(data[node.name + '_out']),
+                              axis=0)
+
+                for j, edge in enumerate(EDGES[i]):
+                    self.edge_inputs[i][j] = \
+                        np.append(self.edge_inputs[i][j], 
+                        pad(data[get_name(edge, sort=False)]),
+                        axis=0)
 
         logging.info("Loaded %d files." % (len(self.file_list),))
+        logging.info("Node Inputs Size: %d" % (len(self.node_inputs[0]),))
 
-    def get_batches(self, is_h, is_train):
-        size = len(self.h_outputs) if is_h else len(self.o_outputs)
+    def get_batches(self, node_ind, is_train):
+        size = len(self.node_inputs[node_ind])
         num_train = int(TRAIN_RATIO * size)
         inds = range(0, size, BATCH_SIZE)[:-1]
         if is_train:
@@ -85,17 +115,15 @@ class Loader(object):
         else:
             inds = range(num_train, size - BATCH_SIZE, BATCH_SIZE)
 
-        if is_h:
-            for ind in inds:
-                end = ind + BATCH_SIZE
-                yield (self.ho_inputs[ind:end], self.ht_inputs[ind:end],
-                       self.h_inputs[ind:end], self.h_outputs[ind:end])
-        else:
-            for ind in inds:
-                end = ind + BATCH_SIZE
-                yield (self.oo_inputs[ind:end], self.oh_inputs[ind:end],
-                       self.ot_inputs[ind:end], self.o_inputs[ind:end],
-                       self.o_outputs[ind:end])
+        inputs = [
+            self.node_inputs[node_ind],
+            self.time_inputs[node_ind],
+            self.node_outputs[node_ind]
+        ] + self.edge_inputs[node_ind]
+
+        for ind in inds:
+            end = ind + BATCH_SIZE
+            yield [x[ind:end] for x in inputs]
 
 def lstm(inputs, num_units):
     """
@@ -125,31 +153,34 @@ def dense(inp, output_size=1024):
     return h
 
 def edgeRNN(inputs):
+    with tf.variable_scope('dense1'):
+        h = dense(inputs, 256)
+
+    with tf.variable_scope('dense2'):
+        h = dense(h, 256)
+
     with tf.variable_scope('lstm1'):
-        h = lstm(inputs, 128)
+        h = lstm(h, 512)
 
     return h
 
-def nodeRNN(inputs, output_size=10):
+def nodeRNN(inputs, output_size=10, skip=None):
     with tf.variable_scope('lstm1'):
-        h = lstm(inputs, 256)
+        h = lstm(inputs, 512)
 
     with tf.variable_scope('dense1'):
+        h = dense(h, 256)
+
+    with tf.variable_scope('dense2'):
+        h = dense(h, 100)
+
+    with tf.variable_scope('dense3'):
         h = dense(h, output_size)
 
+    if skip is not None:
+        h = h + skip
+
     return h
-
-def scores(true, pred):
-    rows = np.where(np.sum(true, 1) > 0)
-    true = np.reshape(np.argmax(true[rows], 1), (-1,))
-    pred = np.reshape(np.argmax(pred[rows], 1), (-1,))
-
-    acc = np.mean(np.equal(true, pred))
-    f1 = f1_score(true, pred, average='weighted')
-    f1_micro = f1_score(true, pred, average='micro')
-    f1_macro = f1_score(true, pred, average='macro')
-    print f1, f1_micro, f1_macro
-    return acc, f1
 
 class SRNN(object):
     def __init__(self, sess, loader):
@@ -157,95 +188,97 @@ class SRNN(object):
         self.loader = loader
 
     def _model(self):
-        with tf.variable_scope('ht'):
-            ht_RNN = edgeRNN(self.ht_inputs)
 
-        with tf.variable_scope('ot'):
-            ot_RNN = edgeRNN(self.ot_inputs)
+        self.nodeRNNs = []
+        self.edgeRNNs = []
+        for i, node in enumerate(NODES):
+            self.edgeRNNs.append([])
+            for j, edge in enumerate(EDGES[i]):
+                with tf.variable_scope(get_name(edge)):
+                    self.edgeRNNs[-1].append(edgeRNN(self.edge_inputs[i][j]))
 
-        with tf.variable_scope('ho'):
-            ho_RNN = edgeRNN(self.ho_inputs)
+            inp = tf.concat(2, 
+                [self.node_inputs[i], self.time_inputs[i]] + self.edgeRNNs[i])
 
-        with tf.variable_scope('oo'):
-            oo_RNN = edgeRNN(self.oo_inputs)
-
-        with tf.variable_scope('H'):
-            inp = tf.concat(2, [ho_RNN, ht_RNN, self.h_inputs])
-            self.H = nodeRNN(inp, output_size=H_OUT_SIZE)
-
-        with tf.variable_scope('O'):
-            inp = tf.concat(2, [oo_RNN, ho_RNN, ot_RNN, self.o_inputs])
-            self.O = nodeRNN(inp, output_size=O_OUT_SIZE)
+            with tf.variable_scope(node.name):
+                self.nodeRNNs.append(nodeRNN(inp, output_size=node.out_size,
+                    skip=self.node_inputs[i]))
 
     def build_model(self):
-        self.ht_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, HT_INP_SIZE], 'ht_inp')
-        self.ot_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, OT_INP_SIZE], 'ot_inp')
-        self.ho_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, HO_INP_SIZE], 'ho_inp')
-        self.oo_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, OO_INP_SIZE], 'oo_inp')
-        self.h_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, H_INP_SIZE], 'h_inp')
-        self.o_inputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, O_INP_SIZE], 'o_inp')
-        self.h_outputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, H_OUT_SIZE], 'h_out')
-        self.o_outputs = tf.placeholder(tf.float32, [BATCH_SIZE, MAX_TIME, O_OUT_SIZE], 'o_out')
+        self.edge_inputs = []
+        self.node_inputs = []
+        self.time_inputs = []
+        self.node_outputs = []
+        for i, node in enumerate(NODES):
+            self.edge_inputs.append([])
+            for edge in EDGES[i]:
+                self.edge_inputs[-1].append(tf.placeholder(tf.float32,
+                    [BATCH_SIZE, MAX_TIME, edge.size], 
+                    get_name(edge, sort=False) + '_inp'))
+
+            self.node_inputs.append(tf.placeholder(tf.float32,
+                [BATCH_SIZE, MAX_TIME, node.inp_size], node.name  + '_inp'))
+            self.time_inputs.append(tf.placeholder(tf.float32,
+                [BATCH_SIZE, MAX_TIME, node.time_size], node.name  + '_inp'))
+            self.node_outputs.append(tf.placeholder(tf.float32,
+                [BATCH_SIZE, MAX_TIME, node.out_size], node.name  + '_inp'))
 
         self._model()
- 
-        self.h_true = tf.reshape(self.h_outputs, [-1, H_OUT_SIZE])
-        self.h_pred = tf.reshape(self.H, [-1, H_OUT_SIZE])
-        self.o_true = tf.reshape(self.o_outputs, [-1, O_OUT_SIZE])
-        self.o_pred = tf.reshape(self.O, [-1, O_OUT_SIZE])
 
-        self.h_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.h_pred, self.h_true))
-        tf.scalar_summary('h_loss', self.h_loss)
-        # self.h_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=BETA_1).minimize(self.h_loss)
-        self.h_optim = tf.train.RMSPropOptimizer(LEARNING_RATE).minimize(self.h_loss)
-        self.o_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.o_pred, self.o_true))
-        tf.scalar_summary('o_loss', self.o_loss)
-        # self.o_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=BETA_1).minimize(self.o_loss)
-        self.o_optim = tf.train.RMSPropOptimizer(LEARNING_RATE).minimize(self.o_loss)
+        self.losses = []
+        self.optims = []
+        for i, node in enumerate(NODES):
+            true = tf.reshape(self.node_outputs[i], [-1, node.out_size])
+            pred = tf.reshape(self.nodeRNNs[i], [-1, node.out_size])
+            loss = tf.reduce_mean(tf.squared_difference(true, pred))
+            self.losses.append(loss)
+            tf.scalar_summary(node.name + '_loss', loss)
+            optim = tf.train.RMSPropOptimizer(LEARNING_RATE).minimize(loss)
+            self.optims.append(optim)
 
         self.merged = tf.merge_all_summaries()
 
-    def run_batches(self, is_h, is_train, start_ind, writer):
-        batches = self.loader.get_batches(is_h, is_train)
-        ind = start_ind
-        loss_sum = 0
-        count = 0
-        if is_h:
-            feed_dict = lambda x: {
-                self.ho_inputs: x[0], 
-                self.ht_inputs: x[1],
-                self.h_inputs: x[2],
-                self.h_outputs: x[3]
-            }
-            loss, true, pred = self.h_loss, self.h_true, self.h_pred
-            arr = [np.zeros((0, H_OUT_SIZE)), np.zeros((0, H_OUT_SIZE))]
-            optim = self.h_optim
-        else:
-            feed_dict = lambda x: {
-                self.oo_inputs: x[0],
-                self.ho_inputs: x[1],
-                self.ot_inputs: x[2],
-                self.o_inputs: x[3],
-                self.o_outputs: x[4]
-            }
-            loss, true, pred = self.o_loss, self.o_true, self.o_pred
-            arr = [np.zeros((0, O_OUT_SIZE)), np.zeros((0, O_OUT_SIZE))]
-            optim = self.o_optim
+    def _get_feed_dicts(self, all_batches):
+        skipped = [False] * len(all_batches)
+        i = 0
+        num_skipped = 0
+        while True:
+            if skipped[i]:
+                num_skipped += 1
+                if num_skipped == len(all_batches):
+                    return
+                continue
 
-        funcs = [true, pred, loss]
-        if is_train:
-            funcs.append(optim)
-        
-        for batch in batches:
-            res = self.sess.run(funcs, feed_dict=feed_dict(batch))
-            arr = [np.append(arr[i], res[i], axis=0) for i in range(2)]
-            loss_sum += res[2]
+            num_skipped = 0
+            batch = next(all_batches[i], False)
+            if batch:
+                inputs = [self.node_inputs[i], self.time_inputs[i],
+                    self.node_outputs[i]] + self.edge_inputs[i]
+                feed_dict = {x:y for x, y in zip(inputs, batch)}
+                yield self.losses[i], self.optims[i], feed_dict
+            else:
+                skipped[i] = True
+
+            i = (i + 1) % len(all_batches)
+
+    def run_batches(self, is_train, start_ind, writer):
+        all_batches = [self.loader.get_batches(i, is_train) for i in range(len(NODES))]
+        ind = start_ind
+        count = 0
+        loss_sum = 0
+
+        for loss, optim, feed_dict in self._get_feed_dicts(all_batches):
+            funcs = [loss]
+            if is_train:
+                funcs.append(optim)
+
+            res = self.sess.run(funcs, feed_dict=feed_dict)
+            loss_sum += res[0]
+
             ind += 1
             count += 1
 
-        acc, f1 = scores(arr[0], arr[1])
-
-        return loss_sum / count, ind, acc, f1
+        return loss_sum / count, ind
 
     def train_model(self):
         train_writer = tf.train.SummaryWriter(os.path.join(LOGS_DIR, 'train'),
@@ -266,28 +299,22 @@ class SRNN(object):
             ind = 0
             for epoch in range(NUM_EPOCHS):
                 logging.info("Begin Epoch %d" % (epoch,))
-                logging.info("Begin H Train")
-                loss, ind, acc, f1 = self.run_batches(True, True, ind, train_writer)
-                logging.info("H Train Loss: %0.4f, Acc: %0.4f, F1: %0.4f" % (loss, acc, f1))
+                logging.info("Begin Train")
+                loss, ind = self.run_batches(True, ind, train_writer)
+                logging.info("Train Loss: %0.4f" % (loss,))
 
-                logging.info("Begin O Train")
-                loss, ind, acc, f1 = self.run_batches(False, True, ind, train_writer)
-                logging.info("O Train Loss: %0.4f, Acc: %0.4f, F1: %0.4f" % (loss, acc, f1))
-
-                logging.info("Begin H Val")
-                loss, ind, acc, f1 = self.run_batches(True, False, ind, val_writer)
-                logging.info("H Val Loss: %0.4f, Acc: %0.4f, F1: %0.4f" % (loss, acc, f1))
-
-                logging.info("Begin O Val")
-                loss, ind, acc, f1 = self.run_batches(False, False, ind, val_writer)
-                logging.info("O Val Loss: %0.4f, Acc: %0.4f, F1: %0.4f" % (loss, acc, f1))
+                logging.info("Begin Val")
+                loss, ind = self.run_batches(False, ind, val_writer)
+                logging.info("Val Loss: %0.4f" % (loss,))
 
                 logging.info("Save Checkpoint")
                 saver.save(sess, CHECKPOINT)
 
 def main():
     sess = tf.Session()
-    file_list = glob.glob(DATA_DIR + '/*')
+    file_list = glob.glob(DATA_DIR + '/*.npz')
+    if MAX_FILES:
+        file_list = file_list[:MAX_FILES]
     logging.info("Loading Dataset")
     loader = Loader(file_list)
     srnn = SRNN(sess, loader)
@@ -301,6 +328,7 @@ if __name__ == '__main__':
     parser.add_argument('--num-epochs', type=int)
     parser.add_argument('--batch-size', type=int)
     parser.add_argument('--use-ckpt', action="store_true")
+    parser.add_argument('--max-files', type=int)
 
     args = parser.parse_args()
     if args.num_epochs:
@@ -309,5 +337,7 @@ if __name__ == '__main__':
         NUM_EPOCHS = args.batch_size
     if args.use_ckpt:
         USE_CHECKPOINT = True
+    if args.max_files:
+        MAX_FILES = args.max_files
 
     main()
